923be8ff2d937a6c71b7036307e81d1bf093e30b
vaishnavipratale <dipakpratale158@gmail.com>
2023-08-07 14:31:39 +0530

    third

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const fs = require("fs");
 
-const cors = require("cors");
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+// const cors = require("cors");
+// app.use(
+//   cors({
+//     origin: "*",
+//   })
+// );
 
 const dotenv = require("dotenv");
 dotenv.config();
@@ -23,14 +23,45 @@ app.use(bodyParser.json());
 
 //Router
 const userRouter = require("./router/userRouter");
+const homePageRouter = require("./router/homePageRouter");
+const chatRouter = require("./router/chatRouter");
+const groupRouter = require("./router/groupRouter");
+
 //Models
+const User = require("./models/userModel");
+const Chat = require("./models/chatModel");
+const Group = require("./models/groupModel");
+const UserGroup = require("./models/userGroup");
+
+//Relationships between Tables
+User.hasMany(Chat, { onDelete: "CASCADE", hooks: true });
+
+Chat.belongsTo(User);
+Chat.belongsTo(Group);
+
+User.hasMany(UserGroup);
+
+Group.hasMany(Chat);
+Group.hasMany(UserGroup);
+
+UserGroup.belongsTo(User);
+UserGroup.belongsTo(Group);
 
 //Middleware
 app.use("/", userRouter);
 app.use("/user", userRouter);
 
+app.use("/homePage", homePageRouter);
+
+app.use("/chat", chatRouter);
+
+app.use("/group", groupRouter);
+
+const job = require("./jobs/cron");
+job.start();
+
 sequelize
-  .sync()
+  .sync({ force: true })
   .then((result) => {
     app.listen(process.env.PORT || 3000);
   })
